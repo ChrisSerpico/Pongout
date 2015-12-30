b8d3d3b68d97189d0234dc335336ecaab36eb4f6
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2015-12-30 14:19:46 -0600

    Made bricks breakable
    -Also changed some sprites around

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+    // Variables for tracking when to break the block
+    public int maxHits;
+    private int timesHit;
+    private bool isBreakable;
+    
+    // Use this for initialization
+	void Start () 
+    {
+        timesHit = 0;
+        isBreakable = (this.tag == "Breakable");
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isBreakable)
+        {
+            HandleHits();
+        }
+    }
+
+    private void HandleHits()
+    {
+        timesHit++;
+        if (timesHit >= maxHits)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
